13b0bd72f75feaedf9b0e35e83ccfcd69aabf791
BabaYaga007j <soumyasourav35@gmail.com>
2023-01-30 18:05:49 +0530

    Added: Switch Case

--- src/com/bz/ImplWageComputation.java
+++ src/com/bz/ImplWageComputation.java
@@ -3,19 +3,53 @@ package com.bz;
 public class ImplWageComputation implements IwageCopmutation
 {
 
-	
-	public void addPartTimeEmployeeWage() {
-		
-			byte perHour = 20;
-			byte fullTime = 8;
-			int employee = (int) (((Math.random())* 100 ));
-			
-			System.out.println("Employee present" + employee);
-			
-			int wage = perHour*fullTime*employee;
-			System.out.println("Total wage perday :: " + wage);
+	public void switchCase() {
+		byte partTime=(byte)(Math.random()*10);
 
+		int wagePerHour=20;
+	
+		System.out.println("Part_Time Hour :: "+partTime);
+		switch(partTime) {
+		case 2:
+			System.out.println("Total number of Part_Time working hour :: "+partTime);
+			System.out.println("Total Wage_per_Day :: "+wagePerHour*partTime);
+			System.out.println("Total Wage_per_Month :: "+wagePerHour*partTime*20);
+			break;
+		case 3:
+			System.out.println("Total number of Part_Time working hour :: "+partTime);
+			System.out.println("Total Wage_per_Day :: "+wagePerHour*partTime);
+			System.out.println("Total Wage_per_Month :: "+wagePerHour*partTime*20);
+			break;
+		case 4:
+			System.out.println("Total number of Part_Time working hour :: "+partTime);
+			System.out.println("Total Wage_per_Day :: "+wagePerHour*partTime);
+			System.out.println("Total Wage_per_Month :: "+wagePerHour*partTime*20);
+			break;
+		case 5:
+			System.out.println("Total number of Part_Time working hour :: "+partTime);
+			System.out.println("Total Wage_per_Day :: "+wagePerHour*partTime);
+			System.out.println("Total Wage_per_Month :: "+wagePerHour*partTime*20);
+			break;
+		case 6:
+			System.out.println("Total number of Part_Time working hour :: "+partTime);
+			System.out.println("Total Wage_per_Day :: "+wagePerHour*partTime);
+			System.out.println("Total Wage_per_Month :: "+wagePerHour*partTime*20);
+			break;
+		case 7:
+			System.out.println("Total number of Part_Time working hour :: "+partTime);
+			System.out.println("Total Wage_per_Day :: "+wagePerHour*partTime);
+			System.out.println("Total Wage_per_Month :: "+wagePerHour*partTime*20);
+			break;
+		case 8:
+			System.out.println("Total number of Part_Time working hour :: "+partTime);
+			System.out.println("Total Wage_per_Day :: "+wagePerHour*partTime);
+			System.out.println("Total Wage_per_Month :: "+wagePerHour*partTime*20);
+			break;
+		default:
+			System.out.println(" :: Invalid Working Hour ::");	
 		}
+		
 	}
+}
 
 		
