5d93e59e068373a7f4839be52b77c62dc4c81f8a
BabaYaga007j <soumyasourav35@gmail.com>
2023-01-30 18:07:12 +0530

    Added: Monthly Wage

--- src/com/bz/EmployeeWageMain.java
+++ src/com/bz/EmployeeWageMain.java
@@ -5,7 +5,7 @@ public class EmployeeWageMain {
 	public static void main(String[] args) {
 		
 		IwageCopmutation computation= new ImplWageComputation();
-		computation.switchCase();
+		computation.calculateMonthlyWage();
 	}
 
 }
--- src/com/bz/IwageCopmutation.java
+++ src/com/bz/IwageCopmutation.java
@@ -2,6 +2,6 @@ package com.bz;
 
 public interface IwageCopmutation {
 	
-	void switchCase();
+	void calculateMonthlyWage();
 
 }
